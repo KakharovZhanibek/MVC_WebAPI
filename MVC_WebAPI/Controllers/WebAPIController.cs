8b2f22f62da2319753b82954f7747b718fd5187e
КахаровЖ <@N215-2>
2020-02-24 21:21:14 +0600

    Add project files.

---
+++ MVC_WebAPI/Controllers/WebAPIController.cs
@@ -0,0 +1,53 @@
+﻿using MVC_WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MVC_WebAPI.Controllers
+{
+    [RoutePrefix("Books")]
+    public class WebAPIController : ApiController
+    {
+        [HttpGet, Route("GetAll")]
+        public IEnumerable<Book> GetAll()
+        {
+            return Repo.Data;
+        }
+        [HttpGet, Route("GetById/{Id}")]
+        public Book GetById(int Id)
+        {
+            return Repo.Data.FirstOrDefault(f => f.Id == Id);
+        }
+        [HttpPost, Route("Add")]
+        public int Add(Book book)
+        {
+            Repo.Data.Add(book);
+            return Repo.Data.Count();
+        }
+
+        [HttpPost, Route("Edit/{Id}")]
+        public string Edit([FromBody]Book book, [FromUri]int Id)
+        {
+            var book2 = Repo.Data.FirstOrDefault(f => f.Id == Id);
+            if (book2 == null)
+            {
+                return null;
+            }
+
+            book2.Name = book.Name;
+            book2.Author = book.Author;
+            book2.Year = book.Year;
+
+            Repo.Data.Add(book);
+            return book2.Name;
+        }
+        [HttpPost, Route("Delete")]
+        public bool Delete([FromUri]int Id)
+        {
+            return Repo.Data.Remove(Repo.Data.FirstOrDefault(f=>f.Id==Id));
+        }
+    }
+}
---
+++ MVC_WebAPI/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_WebAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+    }
+    public class Repo
+    {
+        public static List<Book> Data;
+        public Repo()
+        {
+            Data = new List<Book>();
+
+            Data.Add(new Book { Id = 1, Name = "Book", Author = "Author", Year = 2002 });
+            Data.Add(new Book { Id = 2, Name = "Book", Author = "Author", Year = 2000 });
+            Data.Add(new Book { Id = 3, Name = "Book", Author = "Author", Year = 1998 });
+            Data.Add(new Book { Id = 4, Name = "Book", Author = "Author", Year = 2009 });
+        }
+    }
+}
